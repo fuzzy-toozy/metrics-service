f229ce429f0b4f4c783c513a69ef86dcb93e8358
Nikita Enin <Nikita.Enin@infowatch.com>
2023-09-12 22:07:48 +0300

    Inrement1 implementation

---
+++ internal/server/handlers.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	"fmt"
+	"mime"
+	"net/http"
+	"strings"
+
+	"github.com/fuzzy-toozy/metrics-service/internal/storage"
+)
+
+type MetricRegistryHandler struct {
+	Registry storage.MetricsStorage
+}
+
+func (h *MetricRegistryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	urlTrimmed := strings.Trim(r.URL.Path, "/")
+	url := strings.Split(urlTrimmed, "/")
+	if len(url) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "404 page not found", http.StatusNotFound)
+		return
+	}
+
+	if !hasContentType(r, "text/plain") {
+		http.Error(w, "Unsupported media type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	metricType, metricName, metricValue := url[0], url[1], url[2]
+
+	repo, err := h.Registry.GetRepository(metricType)
+
+	if err != nil {
+		http.Error(w, "Bad metric type", http.StatusBadRequest)
+		return
+	}
+
+	err = repo.AddOrUpdate(metricName, metricValue)
+
+	if err != nil {
+		http.Error(w, "Bad metric value", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	m, _ := repo.Get(metricName)
+	w.Write([]byte(fmt.Sprintf("Metric type '%v', name: '%v', value: '%v' updated. New value: '%v'",
+		metricType, metricName, metricValue, m.GetValue())))
+}
+
+func hasContentType(r *http.Request, mimetype string) bool {
+	contentType := r.Header.Get("Content-type")
+	for _, v := range strings.Split(contentType, ",") {
+		t, _, err := mime.ParseMediaType(v)
+		if err != nil {
+			break
+		}
+		if t == mimetype {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/storage/repository.go
@@ -0,0 +1,169 @@
+package storage
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Metric interface {
+	GetValue() string
+	GetLastTimeUpdated() time.Time
+	SetLastTimeUpdated(t time.Time)
+	UpdateValue(v string) error
+}
+
+type MetricUpdateTime struct {
+	LastTimeUpdated time.Time
+}
+
+func (m MetricUpdateTime) GetLastTimeUpdated() time.Time {
+	return m.LastTimeUpdated
+}
+
+func (m *MetricUpdateTime) SetLastTimeUpdated(t time.Time) {
+	m.LastTimeUpdated = t
+}
+
+type GaugeMetric struct {
+	MetricUpdateTime
+	Val float64
+}
+
+type CounterMetric struct {
+	MetricUpdateTime
+	Val int64
+}
+
+func (m GaugeMetric) GetValue() string {
+	return strconv.FormatFloat(m.Val, 'f', -1, 64)
+}
+
+func (m *GaugeMetric) UpdateValue(v string) error {
+	val, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		return err
+	}
+	m.Val = val
+	return nil
+}
+
+func (m CounterMetric) GetValue() string {
+	return strconv.FormatInt(m.Val, 10)
+}
+
+func (m *CounterMetric) UpdateValue(v string) error {
+	val, err := strconv.ParseInt(v, 10, 64)
+
+	if err != nil {
+		return err
+	}
+
+	m.Val += val
+	return nil
+}
+
+type Repository interface {
+	AddOrUpdate(key string, val string) error
+	Delete(key string) error
+	Get(key string) (Metric, error)
+}
+
+type CommonMetricRepository struct {
+	storage map[string]Metric
+}
+
+func (r *CommonMetricRepository) Delete(key string) error {
+	delete(r.storage, key)
+	return nil
+}
+
+func (r *CommonMetricRepository) Get(key string) (Metric, error) {
+	val, ok := r.storage[key]
+	if !ok {
+		return nil, nil
+	}
+
+	return val, nil
+}
+
+type GaugeMetricRepository struct {
+	CommonMetricRepository
+}
+
+func (r *GaugeMetricRepository) AddOrUpdate(key string, val string) error {
+	return addOrUpdate(key, val, &GaugeMetric{}, r.storage)
+
+}
+
+func NewGaugeMetricRepository() *GaugeMetricRepository {
+	repo := GaugeMetricRepository{}
+	repo.storage = make(map[string]Metric)
+	return &repo
+}
+
+type CounterMetricRepository struct {
+	CommonMetricRepository
+}
+
+func (r *CounterMetricRepository) AddOrUpdate(key string, val string) error {
+	return addOrUpdate(key, val, &CounterMetric{}, r.storage)
+}
+
+func addOrUpdate(key string, val string, m Metric, storage map[string]Metric) error {
+	v, ok := storage[key]
+	if !ok {
+		err := m.UpdateValue(val)
+		if err != nil {
+			return err
+		}
+		m.SetLastTimeUpdated(time.Now())
+		storage[key] = m
+	} else {
+		err := v.UpdateValue(val)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func NewCounterMetricRepository() *CounterMetricRepository {
+	repo := CounterMetricRepository{}
+	repo.storage = make(map[string]Metric)
+	return &repo
+}
+
+type MetricsStorage interface {
+	GetRepository(name string) (Repository, error)
+	AddRepository(name string, repo Repository) error
+	DeleteRepository(name string) error
+}
+
+type CommonMetricsStorage struct {
+	storage map[string]Repository
+}
+
+func (s *CommonMetricsStorage) GetRepository(name string) (Repository, error) {
+	repo, ok := s.storage[name]
+	if !ok {
+		return nil, fmt.Errorf("repository '%v' doesn't exist", name)
+	}
+
+	return repo, nil
+}
+
+func (s *CommonMetricsStorage) AddRepository(name string, repo Repository) error {
+	s.storage[name] = repo
+	return nil
+}
+
+func (s *CommonMetricsStorage) DeleteRepository(name string) error {
+	delete(s.storage, name)
+	return nil
+}
+
+func NewCommonMetricsStorage() *CommonMetricsStorage {
+	storage := CommonMetricsStorage{storage: make(map[string]Repository)}
+	return &storage
+}
